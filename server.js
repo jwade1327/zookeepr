8594c876148601cdab0a0a5d6e32464679a73b3d
unknown <jwade1327@gmail.com>
2020-08-11 13:54:08 -0500

    additional files

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ const { animals } = require('./data/animals');
 
 
 
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+
+app.use(express.static('public'));
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -109,10 +115,9 @@ app.post('/api/animals', (req, res) => {
     }
 });
 
-// parse incoming string or array data
-app.use(express.urlencoded({ extended: true }));
-// parse incoming JSON data
-app.use(express.json());
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
